d9c9c88fefcbdd11ffd3a6e0171ab87dd556c7e0
teesid <teesid@instasim.com>
2023-02-22 23:26:39 +0700

    `zeep-test.py` now uses CachingClient and uses the endpoint url from the env var.

--- zeep-test.py
+++ zeep-test.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
 from zeep import CachingClient
+from zeep.cache import SqliteCache
+from zeep.transports import Transport
 import os
 
 USER = os.environ['MAGENTO_SOAP_USER']
 API_KEY = os.environ['MAGENTO_SOAP_API_KEY']
+MAGENTO_SOAP_WSDL = os.environ.get('MAGENTO_SOAP_WSDL')
 
-client = CachingClient('http://dev.instasim.com/index.php/api/v2_soap?wsdl=1')
+client = CachingClient(
+    MAGENTO_SOAP_WSDL,
+    transport=Transport(
+        cache=SqliteCache(path='./zeep-cache.sqlite')
+    )
+)
 # client.set_ns_prefix(None, 'urn:Magento')
 token = client.service.login(username=USER, apiKey=API_KEY)
 result = client.service.kbankqrInvoiceMany(
